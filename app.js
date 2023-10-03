fd027106a9a54c5d0f5c9eef1ef017e727ffda66
imcateer <mcateerian53@gmail.com>
2023-10-03 02:59:45 +0000

    new or existing user identified when authenticating with AD

--- app.js
+++ app.js
@@ -1,23 +1,59 @@
 const express = require('express');
-const app = express ();
+//const https = require('https');
+const fs = require('fs');
 const cors = require('cors');
+const mysql = require('mysql');
 const ActiveDirectory = require('activedirectory');
+const mongoose = require("mongoose");
 
+const app = express();
+
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+
+var connection = mysql.createConnection({
+	host: '10.1.0.109',
+	user: 'iga',
+	password: 'Maclabs1234',
+	database: 'Users'
+}); 
+
+connection.connect(function(err){
+	if(!err){
+		console.log("DB is connected");
+	}
+	else{
+		console.log(err);
+	}
+});
+
+//var sslRootCAs = require('ssl-root-cas');
+//sslRootCAs.inject(); 
+
+// var privateKey = fs.readFileSync( '/etc/ssl/finalKey.key' )
+// var certificate = fs.readFileSync( '/etc/ssl/finalKey.cer' )
+
+// http.createServer({
+//	key: privateKey,
+//	cert: certificate
+// } , app).listen(3000);
 
 var config = {
 	url: 'ldap://10.1.0.115' ,
 	baseDN: 'dc=corp,dc=maclab,dc=net'
 }; 
+
 var ad = new ActiveDirectory(config);
-
+ 
 app.use(cors({
-	 origin: '*'}));
-app.use(express.json());
+    origin: '*'}));
+    app.use(express.json());
+  
 
-
-app.listen(3000, () => {
+   app.listen(3000, () => {
 	console.log("Server listening on PORT:");
-});
+  });
 
  
 
@@ -29,11 +65,29 @@ app.get("/status", (request, response) => {
 });
 
 app.post("/authenticate", (req, res) => {
-	const {username, password } = req.body;
-
-ad.authenticate(username, password, function(err, auth) {
+  const {username, password} = req.body;
+ad.authenticate(username, password, async function(err, auth) {
 	if (auth) {
-		res.sendStatus(200);
+		console.log(username);
+	        connection.query("SELECT * FROM users WHERE email = '"+username+"'",
+            function(error, result, fields){
+		 if (result.length > 0){
+			 res.send("Existing Account Login");
+			 console.log("existing")
+                 }
+	         else{
+			connection.query("INSERT INTO users (email) VALUES ('"+username+"')",
+		         function(error, results, fields){
+			  if(error){
+			            console.log(error);
+		           }
+			   else{
+		        	res.send("Email did not exist, adding to DB");
+			        console.log("new");
+                           }
+		          }
+			);}
+          });
 	}
 	else{
                 res.sendStatus(401);
